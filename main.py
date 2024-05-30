4359ac29f3687d7b071d518f7d2c40faec843195
Steven Erik Martijn Roest <steven.roest@student.aventus.nl>
2024-05-30 12:17:28 +0200

    New UI, bigger fonts and made the functions better.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# UIFlow import
 from m5stack import *
 from m5stack_ui import *
 from uiflow import *
@@ -6,78 +7,87 @@ screen = M5Screen()
 screen.clean_screen()
 screen.set_screen_bg_color(0x252525)
 
+# -----------------------------------------------------------
 
-huidigVerbruik = 1000000
+# API Verbinding()
+
+# Variabelen
+huidigVerbruik = 199
 highWatt = 200
+weatherGraden = 17
+weatherStad = "Deventer"
+weatherLastUpdated = "10:05"
+weatherKwh = 220
 
-def centerText(text, y_pos, color):
-  text_width = lcd.textWidth(text)
-  screen_width = 320
-  x_pos = int((screen_width - text_width) / 2)
-  lcd.print(text, x_pos, y_pos, color)
+# -----------------------------------------------------------
 
 
-# Describe this function...
+# Functies
+
+# Centreren van tekst
+def centerText(text, y_pos, color, fontsize):
+    text_width = lcd.textWidth(text)
+    screen_width, screen_height = lcd.screensize()  # Haal de breedte en hoogte van het scherm op
+    x_pos = int((screen_width - text_width) / 2)
+    Label = M5Label(text, x=x_pos, y=y_pos, color=color, font=fontsize)
+
+
+# Laat het huidige verbuik zien
 def showHome():
-  global huidigVerbruik, highWatt
-  lcd.clear()
+  screen.clean_screen()
   if huidigVerbruik > highWatt:
-    lcd.fill(0xff6666)
+      screen.set_screen_bg_color(0xff6666)
   else:
-    lcd.fill(0x009900)
-  lcd.print('Huidig Verbruik', 10, 5, 0xffffff)
-  lcd.line(350, 120, 0, 120, 0xffffff)
+    screen.set_screen_bg_color(0x009900)
+  Label = M5Label("Huidig verbuik", 10, 5, 0xffffff, FONT_MONT_22)
+
+  Line = M5Line(350, 120, 0, 120, 0xffffff, 2)
   wattage_text = str(huidigVerbruik) + ' Watt'
-  centerText(wattage_text, 220, 0xffffff)
+  centerText(wattage_text, 215, 0xffffff, FONT_MONT_14)
 
+btnB.wasPressed(showHome)
 
-# Describe this function...
+# Laat het Apparaatbeheer zien
 def showDevices():
-  global huidigVerbruik, highWatt
-  lcd.clear()
-  lcd.print('Apparaatbeheer', 10, 5, 0xffffff)
+  screen.clean_screen()
+  screen.set_screen_bg_color(0x252525)
+  Label = M5Label("Apparaatbeheer", 10, 5, 0xffffff, FONT_MONT_22)
 
-# Describe this function...
+btnC.wasPressed(showDevices)
+
+
+# Laat de instellingen zien
 def showSettings():
-  global huidigVerbruik, highWatt
-  lcd.clear()
-  lcd.print('Instellingen', 10, 5, 0xffffff)
-  lcd.qrcode('Test', x=0, y=0, width=220, version=6)
-
-
-# Describe this function...
-def showWeather():
-  global huidigVerbruik, highWatt
-  lcd.clear()
-  lcd.print('Weer', 10, 5, 0xffffff)
-
-
-def buttonB_pressFor():
+  screen.clean_screen()
+  screen.set_screen_bg_color(0x252525)
+  
+  # Vibration, zodat je weet dat je de instellingen hebt geopend.
   power.setVibrationEnable(True)
   wait(0.2)
   power.setVibrationEnable(False)
-  global huidigVerbruik, highWatt
-  showSettings()
-  pass
-btnB.pressFor(0.8, buttonB_pressFor)
+  
+  Label = M5Label("Instellingen", 10, 5, 0xffffff, FONT_MONT_22)
 
-def buttonA_wasPressed():
-  global huidigVerbruik, highWatt
-  showWeather()
-  pass
-btnA.wasPressed(buttonA_wasPressed)
-
-def buttonC_wasPressed():
-  global huidigVerbruik, highWatt
-  showDevices()
-  pass
-btnC.wasPressed(buttonC_wasPressed)
-
-def buttonB_wasPressed():
-  global huidigVerbruik, highWatt
-  showHome()
-  pass
-btnB.wasPressed(buttonB_wasPressed)
+btnB.pressFor(0.8, showSettings)
 
 
-showHome()
+# Laat het weerbericht zien
+def showWeather():
+  screen.clean_screen()
+  screen.set_screen_bg_color(0x252525)
+  Label = M5Label("Weer", 10, 5, 0xffffff, FONT_MONT_22)
+  
+  weerBox = M5Line(305, 85, 15, 85, 0xffffff, 90)
+  gradenText = str(weatherGraden) + "° C"
+  Graden = M5Label(gradenText, 30, 55, 0x000000, FONT_MONT_22)
+  infoText = str(weatherStad) + "\nUpdated: " + str(weatherLastUpdated)
+  Info = M5Label(infoText, 30, 80, 0x000000, FONT_MONT_14)
+  
+  zonnepaneelBox = M5Line(305, 185, 15, 185, 0xffffff, 90)
+  kwhText = str(weatherKwh) + " kWh"
+  KwH = M5Label(kwhText, 190, 155, 0x000000, FONT_MONT_22)
+  Info = M5Label("Veel stroom opgwekt,\nmaak er gebruik van!", 130, 180, 0x000000, FONT_MONT_14)
+
+btnA.wasPressed(showWeather)
+
+showHome() # Default action, laat op het begin het homescreen zien
